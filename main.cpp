6e8613f5f7fde63991322633875530125ea6363c
Kairo2234 <kacperernt@icloud.com>
2024-01-29 13:08:44 +0100

    ab

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 
+int Policz (int a, int b) {
+    return a+b;
+}
+
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    std:: cout <<"Wynik to " << Policz(2, 3) <<std::endl;
+
     return 0;
 }
